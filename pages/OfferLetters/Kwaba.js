6424ea06f538268516e71ef684466d4ef1ad2fa1
joshnwosu <joshuanwosu078@gmail.com>
2021-08-03 02:10:57 +0100

    init

---
+++ pages/OfferLetters/Kwaba.js
@@ -0,0 +1,10 @@
+import React, {useEffect, useState} from 'react';
+import {View, Text} from 'react-native';
+
+export default function KwabaLetter({navigation}) {
+  return (
+    <View>
+      <Text>Kwaba Letter</Text>
+    </View>
+  );
+}
